bb55132392afd2e34ad6ca46e5b1ea5204d2cafa
Abdullah GÖKALP <gokalppabdullah@gmail.com>
2025-10-30 17:20:53 +0300

    Add project files.

---
+++ ApiProjeKampi.WebApi/Entities/Service.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiProjeKampi.WebApi.Entities
+{
+    public class Service
+    {
+        public int ServiceId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string İconUrl { get; set; }
+
+
+    }
+}
