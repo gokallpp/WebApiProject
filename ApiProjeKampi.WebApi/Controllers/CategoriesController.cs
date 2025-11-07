2b32ad907636ceafbb3af0ca39d429388511e43a
Abdullah GÖKALP <gokalppabdullah@gmail.com>
2025-11-07 23:22:58 +0300

    chef controller

--- ApiProjeKampi.WebApi/Controllers/CategoriesController.cs
+++ ApiProjeKampi.WebApi/Controllers/CategoriesController.cs
@@ -16,6 +16,21 @@ namespace ApiProjeKampi.WebApi.Controllers
             _context = context;
         }
 
+
+        
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+       
+
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var values = _context.Categories.ToList();
+            return Ok(values);
+
+
+
+        }
+
         [HttpPost]
         public IActionResult CreateCategory(Category category)
         {
@@ -23,5 +38,35 @@ namespace ApiProjeKampi.WebApi.Controllers
             _context.SaveChanges();
             return Ok("Kategori Ekleme İşlemi Başarılı");
         }
+
+
+        [HttpDelete]
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            _context.Categories.Remove(value);
+            _context.SaveChanges();
+            return Ok("Kategori Silme Başarılı");
+        }
+
+
+        [HttpGet("GetCategory")]
+
+        public IActionResult GetCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            return Ok(value);
+        }
+
+
+        [HttpPut]
+        public IActionResult UpdateCategory(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            return Ok("Kategori Güncelleme Başarılı");
+        }
+
+
     }
 }
